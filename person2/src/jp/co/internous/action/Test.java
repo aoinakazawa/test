fba4c12f2c9d1569882b9a91a6fc7d06da5514d0
yuki2971 <yukisfuture@gmail.com>
2018-12-10 21:38:46 +0900

    12/10

--- person2/src/jp/co/internous/action/Test.java
+++ person2/src/jp/co/internous/action/Test.java
@@ -15,6 +15,23 @@ public class Test {
 		Person jiro=new Person("jiro",20);
 		System.out.println(jiro.name);
 		System.out.println(jiro.age);
+
+
+		Person saburo=new Person("saburo");
+	    System.out.println(saburo.name);
+	    System.out.println(saburo.age);
+
+
+	    Person 名前なし=new Person(25);
+	    System.out.println(名前なし.name);
+	    System.out.println(名前なし.age);
+
+	    Person hanako=new Person(17,"hanako");
+	    System.out.println(hanako.name);
+	    System.out.println(hanako.age);
+
+
+
 	}
 
 }
